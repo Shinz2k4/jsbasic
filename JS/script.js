dc46c1e718b5cc825c8c152a909c61d8474e8e15
Shinz2k4 <dminh2k4cn@gmail.com>
2024-07-27 23:30:19 +0700

    Clock

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,34 @@
-alert("Hello world from JavaScript")
+function getTime() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let day = currentTime.getDate();
+    let month = currentTime.getMonth() + 1; // Tháng cần cộng thêm 1
+    let year = currentTime.getFullYear();
 
-console.log("Hello world from haha") 
+    // Định dạng giờ và phút để luôn có hai chữ số
+    let formattedHours = hours < 10 ? '0' + hours : hours;
+    let formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    let formattedSeconds = seconds < 10? '0' + seconds : seconds;
 
-let a = 5
-let b = 4.9
-let x = 5 + "hello"
+    // Định dạng ngày, tháng và năm
+    let formattedDay = day < 10 ? '0' + day : day;
+    let formattedMonth = month < 10 ? '0' + month : month;
 
-console.log(a + x)
+    let TimeOclock = formattedHours + ":" + formattedMinutes +":"+ formattedSeconds +"<br>"+
+                     formattedDay + "-" + formattedMonth + "-" + year;
+    return TimeOclock;
+}
+
+console.log(getTime());
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    function updateClock() {
+    document.getElementById("oclock").innerHTML = getTime();
+    
+}
+setInterval(updateClock,1000);
+updateClock();
+});
